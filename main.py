8e4f872e243b819c5aec3f3491328e4ac353cd6a
Santhoshexp <53528710+Santhoshexp@users.noreply.github.com>
2024-06-07 14:41:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,24 +14,19 @@ class HFMODEL():
 
     def fetch_models(self):
         """Method to fetch the specified no of models"""
-        count =0
-        models = list_models()
+        models = list_models(filter='medical')
         columns = ['Model_id','Author','Library_Name','Likes','Last_updated','Downloads']
         res_dfs = []
         for model in models:
-            if count == 10:
-                break
             res_dfs.append(pd.DataFrame([[model.id,model.author, model.library_name, model.likes,model.last_modified ,model.downloads]]
                           ,columns=columns))
-            
-            count += 1
-        print(pd.concat(res_dfs).sort_values('Downloads',ascending=False))
+        print(pd.concat(res_dfs).sort_values('Downloads',ascending=False).head(10))
+
+
+
+
 
 if __name__=='__main__':
     while True:
         HFMODEL().fetch_models()
         ts.sleep(60)
-
-
-
-
